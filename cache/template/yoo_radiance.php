93b15b1f8b514161b8b39148d5bd491c2ccccb3c
Sergii Melnichenko <sergiimelnichenko@Sergiis-MacBook-Pro.local>
2017-03-23 23:25:26 +0200

    Widgetkit and slideshow added

---
+++ cache/template/yoo_radiance.php
@@ -0,0 +1,1 @@
+{"tmpl_check":"4f178d4f380ab23639e5da01e84d9158","tmpl_data":"{\"status\":\"up-to-date\",\"message\":\"You're up to date!. 1.0.4 is currently the newest version available.\"}","warp_check":"8345665cd6e8b947826b89810af0e759","warp_data":"{\"status\":\"up-to-date\",\"message\":\"You're up to date!. 6.4.8 is currently the newest version available.\"}"}
